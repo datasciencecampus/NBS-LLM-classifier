fd8fc698677fa5c53ebedc4b4bba6cc55c1e7a0c
rcatlord <12719575+rcatlord@users.noreply.github.com>
2026-03-12 09:52:58 +0000

    Add post-processing to search-vectorstore.py

--- demo/src/04-search-vectorstore.py
+++ demo/src/04-search-vectorstore.py
@@ -1,4 +1,4 @@
-# 04 - search vectorstore #
+# Search vectorstore
 
 # Load libraries
 import pandas as pd
@@ -18,7 +18,7 @@ vectoriser = MyNormalisedHF_Vectoriser(
   model_kwargs={"trust_remote_code":True}
 )
 
-# ISCO -----------------------------------------------------------------------
+# ISCO ----------------------------------------------------------------------------
 # Load vector store
 from classifai.indexers import VectorStore
 vector_store = VectorStore.from_filespace('../vector_store/isco', vectoriser)
@@ -34,10 +34,27 @@ search_results = vector_store.search(
   search_input, 
   n_results=15
   )
-# Write results
-search_results.to_csv('../data/isco_search_results.csv', index=False)
 
-# ISIC -----------------------------------------------------------------------
+# Post-processing
+search_results_scores = search_results[['query_id','doc_id','score']].groupby(['query_id','doc_id'])['score'].max().reset_index()
+search_results_unique = pd.DataFrame(search_results[['query_id','doc_id']].groupby(['query_id'])['doc_id'].value_counts()).reset_index().drop('count', axis=1)
+search_results_unique = search_results_unique.merge(search_results_scores, left_on=['query_id', 'doc_id'], right_on=['query_id', 'doc_id'])
+search_results_unique['rank'] = search_results_unique.groupby('query_id')['doc_id'].cumcount()
+search_results_0 = search_results_unique[search_results_unique['rank'] == 0].rename(columns={'doc_id': 'prediction_1'})
+search_results_1 = search_results_unique[search_results_unique['rank'] == 1].rename(columns={'doc_id': 'prediction_2'})
+search_results_2 = search_results_unique[search_results_unique['rank'] == 2].rename(columns={'doc_id': 'prediction_3'})
+combined_results = search_results_0.merge(search_results_1[['query_id','prediction_2']], on='query_id', how='left')
+combined_results = combined_results.merge(search_results_2[['query_id','prediction_3']], on='query_id', how='left')
+combined_results = combined_results[['query_id','prediction_1','score','prediction_2','prediction_3']]
+combined_results['prediction_2'] = combined_results['prediction_2'].astype('Int64')
+combined_results['prediction_3'] = combined_results['prediction_3'].astype('Int64')
+validated = pd.read_csv('../data/query_isco.csv', dtype = str)
+matched_results = validated.merge(combined_results, left_on='id', right_on='query_id')
+matched_results['Match top 1'] = matched_results['validated'] == matched_results['prediction_1']
+# Write results
+matched_results.to_csv('../data/search_results_isco.csv', index=False)
+
+# ISIC ----------------------------------------------------------------------------
 # Load vector store
 vector_store = VectorStore.from_filespace('../vector_store/isic', vectoriser)
 # Load query
@@ -51,5 +68,22 @@ search_results = vector_store.search(
   search_input, 
   n_results=15
   )
+
+# Post-processing
+search_results_scores = search_results[['query_id','doc_id','score']].groupby(['query_id','doc_id'])['score'].max().reset_index()
+search_results_unique = pd.DataFrame(search_results[['query_id','doc_id']].groupby(['query_id'])['doc_id'].value_counts()).reset_index().drop('count', axis=1)
+search_results_unique = search_results_unique.merge(search_results_scores, left_on=['query_id', 'doc_id'], right_on=['query_id', 'doc_id'])
+search_results_unique['rank'] = search_results_unique.groupby('query_id')['doc_id'].cumcount()
+search_results_0 = search_results_unique[search_results_unique['rank'] == 0].rename(columns={'doc_id': 'prediction_1'})
+search_results_1 = search_results_unique[search_results_unique['rank'] == 1].rename(columns={'doc_id': 'prediction_2'})
+search_results_2 = search_results_unique[search_results_unique['rank'] == 2].rename(columns={'doc_id': 'prediction_3'})
+combined_results = search_results_0.merge(search_results_1[['query_id','prediction_2']], on='query_id', how='left')
+combined_results = combined_results.merge(search_results_2[['query_id','prediction_3']], on='query_id', how='left')
+combined_results = combined_results[['query_id','prediction_1','score','prediction_2','prediction_3']]
+combined_results['prediction_2'] = combined_results['prediction_2'].astype('Int64')
+combined_results['prediction_3'] = combined_results['prediction_3'].astype('Int64')
+validated = pd.read_csv('../data/query_isic.csv', dtype = str)
+matched_results = validated.merge(combined_results, left_on='id', right_on='query_id')
+matched_results['Match top 1'] = matched_results['validated'] == matched_results['prediction_1']
 # Write results
-search_results.to_csv('../data/isic_search_results.csv', index=False)
+matched_results.to_csv('../data/search_results_isic.csv', index=False)
--- demo/src/05-evaluate.py
+++ demo/src/05-evaluate.py
@@ -1,4 +1,4 @@
-# 05 - evaluate #
+# Evaluate
 
 # Load libraries
 import pandas as pd
@@ -8,27 +8,10 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import FormatStrFormatter
 
 # ISCO ---------------------------------------------------------
-
-# Find top 3 search results and join with validated codes
-search_results = pd.read_csv('../data/isco_search_results.csv')
-search_results_scores = search_results[['query_id','doc_id','score']].groupby(['query_id','doc_id'])['score'].max().reset_index()
-search_results_unique = pd.DataFrame(search_results[['query_id','doc_id']].groupby(['query_id'])['doc_id'].value_counts()).reset_index().drop('count', axis=1)
-search_results_unique = search_results_unique.merge(search_results_scores, left_on=['query_id', 'doc_id'], right_on=['query_id', 'doc_id'])
-search_results_unique['rank'] = search_results_unique.groupby('query_id')['doc_id'].cumcount()
-search_results_0 = search_results_unique[search_results_unique['rank'] == 0].rename(columns={'doc_id': 'prediction_1'})
-search_results_1 = search_results_unique[search_results_unique['rank'] == 1].rename(columns={'doc_id': 'prediction_2'})
-search_results_2 = search_results_unique[search_results_unique['rank'] == 2].rename(columns={'doc_id': 'prediction_3'})
-combined_results = search_results_0.merge(search_results_1[['query_id','prediction_2']], on='query_id', how='left')
-combined_results = combined_results.merge(search_results_2[['query_id','prediction_3']], on='query_id', how='left')
-combined_results = combined_results[['query_id','prediction_1','score','prediction_2','prediction_3']]
-combined_results['prediction_2'] = combined_results['prediction_2'].astype('Int64')
-combined_results['prediction_3'] = combined_results['prediction_3'].astype('Int64')
-validated = pd.read_csv('../data/query_isco.csv')
-test_results = validated.merge(combined_results, left_on='id', right_on='query_id')
-test_results['Match top 1'] = test_results['validated'] == test_results['prediction_1']
+search_results = pd.read_csv('../data/search_results_isco.csv')
 
 # Overall accuracy
-accuracy = (test_results['prediction_1'] == test_results['validated']).mean()*100
+accuracy = (search_results['prediction_1'] == search_results['validated']).mean()*100
 print(f'In {round(accuracy,1)}% of cases the predicted code matched the validated code.')
 
 # Plot Coverage vs Accuracy
@@ -36,8 +19,8 @@ thresholds = np.arange(0, 1.05, 0.01)
 results = []
 
 for threshold in thresholds:
-    covered = test_results.loc[test_results['score'] > threshold]
-    coverage = len(covered) / len(test_results)
+    covered = search_results.loc[search_results['score'] > threshold]
+    coverage = len(covered) / len(search_results)
     accuracy = (covered['validated'] == covered['prediction_1']).mean()
     results.append({'threshold': threshold, 'coverage': coverage, 'accuracy': accuracy})
 
@@ -66,7 +49,63 @@ plt.axhline(y=y1, color='#6a3d9a', ls=':', lw = 2, alpha = 0.8)
 plt.plot(x0, y0, marker='o', color='#cab2d6')
 plt.plot(x1, y1, marker='o', color='#6a3d9a')
 plt.title('Coverage vs. Accuracy for Different Similarity Thresholds', fontsize=14, weight='bold', y=1.055, loc='left')
-plt.gcf().text(0.125, 0.9, 'Q2 2024 NLFS - (sentence-transformers/all-MiniLM-L6-v2)', fontsize=9, color='#666')
+plt.gcf().text(0.125, 0.9, 'ISCO Q2 2024 NLFS - (sentence-transformers/all-MiniLM-L6-v2)', fontsize=9, color='#666')
+plt.xlabel('Coverage')
+plt.ylabel('Accuracy')
+plt.text(0.02, 0.04, 
+         f'cov≈0.50> (thr={z0:.2f})\ncov≈0.80> (thr={z1:.2f})', 
+         transform=plt.gca().transAxes,
+         fontsize=10, color='black',
+         bbox=dict(facecolor='white', edgecolor='gray', boxstyle='round,pad=0.6'))
+plt.gca().yaxis.set_major_formatter(FormatStrFormatter('% 1.2f'))
+plt.gca().yaxis.set_ticks_position('none')
+plt.gca().yaxis.tick_right()
+plt.gca().yaxis.set_label_position('right')
+plt.show()
+
+# ISIC ---------------------------------------------------------
+search_results = pd.read_csv('../data/search_results_isic.csv')
+
+# Overall accuracy
+accuracy = (search_results['prediction_1'] == search_results['validated']).mean()*100
+print(f'In {round(accuracy,1)}% of cases the predicted code matched the validated code.')
+
+# Plot Coverage vs Accuracy
+thresholds = np.arange(0, 1.05, 0.01)
+results = []
+
+for threshold in thresholds:
+    covered = search_results.loc[search_results['score'] > threshold]
+    coverage = len(covered) / len(search_results)
+    accuracy = (covered['validated'] == covered['prediction_1']).mean()
+    results.append({'threshold': threshold, 'coverage': coverage, 'accuracy': accuracy})
+
+results_df = pd.DataFrame(results)
+
+x = results_df['coverage']
+y = results_df['accuracy']
+z = results_df['threshold']
+xs = np.sort(x)
+ys = np.array(y)[np.argsort(x)]
+zs = np.array(z)[np.argsort(x)]
+x0 = 0.5
+x1 = 0.8
+y0 = np.interp(x0, xs, ys)
+y1 = np.interp(x1, xs, ys)
+z0 = np.interp(x0, xs, zs)
+z1 = np.interp(x1, xs, zs)
+
+sns.set_style('whitegrid', {'axes.grid' : False})
+plt.figure(figsize=(10, 6))
+sns.lineplot(x='coverage', y='accuracy', data=results_df, color='#212121')
+plt.axvline(x=0.5, color='#cab2d6', ls=':', lw = 2, alpha = 0.8)
+plt.axvline(x=0.8, color='#6a3d9a', ls=':', lw = 2, alpha = 0.8)
+plt.axhline(y=y0, color='#cab2d6', ls=':', lw = 2, alpha = 0.8)
+plt.axhline(y=y1, color='#6a3d9a', ls=':', lw = 2, alpha = 0.8)
+plt.plot(x0, y0, marker='o', color='#cab2d6')
+plt.plot(x1, y1, marker='o', color='#6a3d9a')
+plt.title('Coverage vs. Accuracy for Different Similarity Thresholds', fontsize=14, weight='bold', y=1.055, loc='left')
+plt.gcf().text(0.125, 0.9, 'ISIC Q2 2024 NLFS - (sentence-transformers/all-MiniLM-L6-v2)', fontsize=9, color='#666')
 plt.xlabel('Coverage')
 plt.ylabel('Accuracy')
 plt.text(0.02, 0.04, 
